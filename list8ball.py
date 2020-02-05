7ed0b324527d2508a7ebd50b664af4ef96c6b342
brendend <brendendemetroff1@gmail.com>
2020-02-05 15:27:05 -0500

    improved list 8 ball to loop through the list

--- list8ball.py
+++ list8ball.py
@@ -5,11 +5,16 @@ messages = ['It is certain.',
     'Yes definitely.',
     'Reply is hazy, try agian.',
     'Ask again later',
-    'Concentrat and ask again.',
+    'Concentrate and ask again.',
     'My reply is no.',
     'Outlook is not good.',
     'Absolutely not.',
     'Fuck no.']
-print('Ask the magic 8 ball a question')
-question = input()
-print(messages[random.randint(0, len(messages) - 1)])
+
+
+while True:   
+        print('Ask the magic 8 ball a question')
+        question = input()
+        print(messages[random.randint(0, len(messages) - 1)])
+        if question == "end":
+            break
